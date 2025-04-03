0d341218684b8df20681cdaaac177cffcb9f4ba6
prashant_vegada <prashantvegada7@gmail.com>
2025-04-03 12:05:09 -0700

    useRef hook add

--- TodoApp/src/Components/AddTodo.jsx
+++ TodoApp/src/Components/AddTodo.jsx
@@ -1,51 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import css from "./AddTodo.module.css";
 
 const AddTodo = ({ onNewItem }) => {
   const [todoName, setTodoName] = useState();
   const [dueDate, setDueDate] = useState();
 
-  const handleChangeName = () => {
-    setTodoName(event.target.value);
-  };
+  const todoNameElement = useRef();
+  const todoDateElement = useRef();
 
-  const handleDateName = () => {
-    setDueDate(event.target.value);
-  };
-
-  const handleAddButton = () => {
+  const handleAddButton = (event) => {
+    event.preventDefault();
+    let todoName = todoNameElement.current.value;
+    let dueDate = todoDateElement.current.value;
+    todoNameElement.current.value = "";
+    todoDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
   };
 
   return (
-    <div className="container  mb-3">
+    <form className="container  mb-3" onSubmit={handleAddButton}>
       <div className="row">
         <div className="col-6">
           <input
             type="text"
             placeholder="Enter items here..."
             className={css.xInput}
-            value={todoName}
-            onChange={handleChangeName}
+            ref={todoNameElement}
           />
         </div>
         <div className="col-4">
-          <input
-            type="date"
-            className={css.xInput}
-            value={dueDate}
-            onChange={handleDateName}
-          />
+          <input type="date" className={css.xInput} ref={todoDateElement} />
         </div>
         <div className="col-2">
-          <button className="btn btn-success xButton" onClick={handleAddButton}>
+          <button type="submit" className="btn btn-success xButton">
             Add
           </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
